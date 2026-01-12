c34985f9757f14596435596269939b3a973dbdb4
momentics <momentics@gmail.com>
2026-01-12 19:49:04 +0300

    LUT color map table

---
+++ include/measlib/ui/colors.h
@@ -0,0 +1,18 @@
+/**
+ * @file colors.h
+ * @brief Pre-defined Color Maps for Heatmaps/Waterfalls.
+ *
+ * @author Architected by momentics <momentics@gmail.com>
+ * @copyright (c) 2026 momentics
+ */
+
+#ifndef MEASLIB_UI_COLORS_H
+#define MEASLIB_UI_COLORS_H
+
+#include "measlib/ui/render.h"
+
+// Available Color Maps
+extern const meas_color_map_t meas_cmap_hot;
+extern const meas_color_map_t meas_cmap_jet;
+
+#endif // MEASLIB_UI_COLORS_H
